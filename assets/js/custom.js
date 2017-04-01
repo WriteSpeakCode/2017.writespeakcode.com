73e1c85db2c1696b343005d98e95459d901d68e8
Nicola Beuscher <nbeuscher@gmail.com>
2017-04-01 11:20:57 -0400

    Allow click events on map once you click the iframe
    - Prevents map from stopping the page scrolling when the cursor hits
    the map

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,4 +3,14 @@ $(document).ready(function() {
   $('#qcSiteNav a').click(function() {
     $('#qcSiteNav').fadeOut();
   });
+
+// Enable events on map once you click map
+  $('.venue-info-map').click(function () {
+    $('.venue-info-map iframe').css("pointer-events", "auto");
+  });
+
+  $( ".venue-info-map" ).mouseleave(function() {
+    $('.venue-info-map iframe').css("pointer-events", "none");
+  });
+
 });
