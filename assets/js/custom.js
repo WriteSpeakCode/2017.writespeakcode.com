4aa066e7491be426f04e36f12cd953c95a3663d8
Jillian Campbell <jill.campbell07@gmail.com>
2017-04-02 16:40:24 -0400

    Add offset space for sticky nav once position changes from absolute to fix.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -8,13 +8,15 @@ $(document).ready(function() {
 //sticky desktop nav bar
 $(function() {
     //sticky nav located under coutdown, sticks to top on scroll
-    var navBar = $(".middleHeader");
+    var navBar = $('.middleHeader');
     $(window).scroll(function() {
         var scroll = $(window).scrollTop();
         if (scroll >= 850) {
-            navBar.removeClass('middleHeader').addClass("stickyHeader");
+            navBar.removeClass('middleHeader').addClass('stickyHeader');
+            $('#schedule').addClass('addSpace')
         } else {
-            navBar.removeClass("stickyHeader").addClass('middleHeader');
+            navBar.removeClass('stickyHeader').addClass('middleHeader');
+            $('#schedule').removeClass('addSpace')
         }
     });
 });
