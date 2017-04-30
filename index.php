5206ebe35071d226035154d361b9937f25316352
J Armstrong <jarmstrng@users.noreply.github.com>
2017-04-30 19:13:36 -0400

    WIP: attempt to redirect to https (#60)

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <!-- This is a hack to deploy a static site to Heroku
 http://blog.teamtreehouse.com/deploy-static-site-heroku -->
 
-<?php header( 'Location: /index.html' ) ;  ?>
+<?php header("Location: https://" . $_SERVER["HTTP_HOST"] . "/index.html");  ?>
